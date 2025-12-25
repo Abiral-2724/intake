59c264983d46d643c2dc0ddeaabb08979acede5b
Abiral-2724 <abiral2004jain@gmail.com>
2025-12-25 16:13:31 +0530

    index.js at backend initilize

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,24 @@
-console.log("hello")
+import express from 'express' 
+import cors from 'cors' ; 
+import dotenv from 'dotenv' ; 
+
+
+const app = express() ; 
+
+dotenv.config({}) ; 
+
+const PORT = process.env.PORT || 8000 ; 
+
+app.use(express.json()) ;
+app.use(express.urlencoded({extended : true})) ; 
+
+const corsOptions = {
+    origin: 'http://localhost:3002',
+    credentials: true, // Enable credentials (cookies)
+};
+
+app.use(cors(corsOptions)) ; 
+
+app.listen(PORT ,() => {
+    console.log(`server running at port ${PORT}`) ; 
+})
