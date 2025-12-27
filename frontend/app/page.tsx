aa18145406952cfef66a60686e81de1e5cfcc42e
Abiral-2724 <abiral2004jain@gmail.com>
2025-12-27 22:12:03 +0530

    frontend basic component added

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,37 @@
+import ComingSoon from "@/components/ComingSoon";
+import CTASection from "@/components/CTASection";
+import { FAQSection } from "@/components/FAQSection";
+import { Footer } from "@/components/Footer";
+import { HandWrittenTitle } from "@/components/HandWrittenText";
+import LoaderOne from "@/components/Loader-one";
+import { LoaderTwo} from "@/components/Loader-two";
+import LoginPage from "@/components/Login";
+import PricingSection from "@/components/PricingSection";
+import { BankingScaleHero } from "@/components/ScaleHero";
+import TestimonialsSection from "@/components/testimonials";
+import { Button } from "@/components/ui/button";
+import { HeroSection } from "@/components/Wishlist";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div className="bg-red-900">
-    hello
+   <div>
+    <HeroSection
+  distortion={1.2}
+  speed={0.8}
+/>;
+    <HandWrittenTitle title="Kokonut UI" subtitle="Optional subtitle" ></HandWrittenTitle>
+    <LoaderTwo></LoaderTwo>
+    <LoaderOne></LoaderOne>
+    <ComingSoon></ComingSoon>
+    <LoginPage></LoginPage>
+    <BankingScaleHero></BankingScaleHero>
+    <CTASection></CTASection>
+    <FAQSection></FAQSection>
+    
+    <PricingSection></PricingSection>
+    <TestimonialsSection></TestimonialsSection>
+    <Footer></Footer>
    </div>
   );
 }
