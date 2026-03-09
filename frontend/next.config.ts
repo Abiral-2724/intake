49605cee6a63088cf63cb21ed6ee88350568707b
Abiral-2724 <abiral2004jain@gmail.com>
2026-03-09 17:38:13 +0530

    change1

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // Uncomment if you are using server actions, otherwise remove it
+    // serverActions: {},
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+    
+  },
 };
 
 export default nextConfig;
